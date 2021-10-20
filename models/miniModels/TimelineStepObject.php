1d7dbd12962a0666b1e03b19f34e15a7b37ef2f6
Абдурашидов Тимур <tim-a@realtor.ru>
2021-10-20 16:02:17 +0300

    add comment column in objects table and github actions inslude

--- models/miniModels/TimelineStepObject.php
+++ models/miniModels/TimelineStepObject.php
@@ -16,6 +16,7 @@ use Yii;
  * @property int|null $option Дополнительные флаги для объекта
  * @property string|null $created_at
  * @property string|null $updated_at
+ * @property string|null $comment
  * @property int|null $type_id Херня для API
  *
  * @property TimelineStep $timelineStep
@@ -38,6 +39,7 @@ class TimelineStepObject extends \yii\db\ActiveRecord
         return [
             [['timeline_step_id', 'object_id', 'offer_id'], 'required'],
             [['timeline_step_id', 'object_id', 'offer_id', 'status', 'option', 'type_id', 'complex_id'], 'integer'],
+            [['comment'], 'string'],
             [['created_at', 'updated_at'], 'safe'],
             [['timeline_step_id'], 'exist', 'skipOnError' => true, 'targetClass' => TimelineStep::className(), 'targetAttribute' => ['timeline_step_id' => 'id']],
         ];
@@ -58,6 +60,7 @@ class TimelineStepObject extends \yii\db\ActiveRecord
             'option' => 'Option',
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
+            'comment' => 'Comment',
             'type_id' => 'Type ID',
         ];
     }
