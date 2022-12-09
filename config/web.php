fdf88474db1c762fd066528328bce2fb87a153f8
Timur <billypro6@gmail.com>
2022-12-09 15:51:15 +0300

    change web config

--- config/web.php
+++ config/web.php
@@ -19,13 +19,13 @@ $config = [
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
+if (YII_ENV != "prod") {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ["*"],
     ];
 
     $config['bootstrap'][] = 'gii';
