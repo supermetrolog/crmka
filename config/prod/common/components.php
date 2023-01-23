ca8cbca690e65d3d14b13c5702918e80b5aae6f9
Timur <billypro6@gmail.com>
2023-01-23 15:08:16 +0300

    fix prod config bug

--- config/prod/common/components.php
+++ config/prod/common/components.php
@@ -5,10 +5,12 @@ $secrets = require __DIR__ . "/../../secrets.php";
 return [
     'log' => [
         'targets' => [
-            'class' => 'airani\log\TelegramTarget',
-            'levels' => ['error'],
-            'botToken' => $secrets['tg_logger_bot']['token'], // bot token secret key
-            'chatId' => $secrets['tg_logger_bot']['channel'], // chat id or channel username with @ like 12345 or @channel
+            [
+                'class' => 'airani\log\TelegramTarget',
+                'levels' => ['error'],
+                'botToken' => $secrets['tg_logger_bot']['token'], // bot token secret key
+                'chatId' => $secrets['tg_logger_bot']['channel'], // chat id or channel username with @ like 12345 or @channel
+            ]
         ]
     ],
 
