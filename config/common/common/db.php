7c8490a3e469d04423f112ac49790db41f441d32
supermetrolog <billypro6@gmail.com>
2023-03-26 16:26:34 +0300

    configure docker-compose

--- config/common/common/db.php
+++ config/common/common/db.php
@@ -4,7 +4,7 @@ $secrets = require __DIR__ . "/../../secrets.php";
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=' . $secrets['db']['dbname'],
+    'dsn' => 'mysql:host=' . $secrets['db']['host'] . ';dbname=' . $secrets['db']['dbname'],
     'username' => $secrets['db']['username'],
     'password' => $secrets['db']['password'],
     'charset' => 'utf8',
--- config/common/common/db_old.php
+++ config/common/common/db_old.php
@@ -4,7 +4,7 @@ $secrets = require __DIR__ . "/../../secrets.php";
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=' . $secrets['db_old']['dbname'],
+    'dsn' => 'mysql:host=' . $secrets['db_old']['host'] . ';dbname=' . $secrets['db_old']['dbname'],
     'username' => $secrets['db_old']['username'],
     'password' => $secrets['db_old']['password'],
     'charset' => 'utf8',
