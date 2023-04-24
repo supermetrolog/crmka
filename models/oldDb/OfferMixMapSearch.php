5e947b60e3d51f76e181f6e1c482220b5bc7d868
supermetrolog <billypro6@gmail.com>
2023-04-24 22:10:53 +0300

    refactored offers mix search models

--- models/oldDb/OfferMixMapSearch.php
+++ models/oldDb/OfferMixMapSearch.php
@@ -13,29 +13,6 @@ use yii\helpers\ArrayHelper;
 class OfferMixMapSearch extends OfferMixSearch
 {
     /**
-     *  @var string|array
-     */
-    public $polygon;
-
-    /**
-     * @return array
-     */
-    public function rules(): array
-    {
-        return ArrayHelper::merge(parent::rules(), [
-            ['polygon', 'string']
-        ]);
-    }
-
-    /**
-     * @return string
-     */
-    protected function getTableName(): string
-    {
-        return OfferMix::tableName();
-    }
-
-    /**
      * @return ActiveQuery
      */
     private function getQuery(): ActiveQuery
@@ -57,67 +34,6 @@ class OfferMixMapSearch extends OfferMixSearch
             ->search()
             ->select($select)
             ->groupBy($this->getField('object_id'));
-    }
-
-    /**
-     * @return void
-     */
-    public function normalizePolygon(): void
-    {
-        $polygon = $this->stringToArray($this->polygon);
-        if (!$polygon || !is_array($polygon) || count($polygon) % 2 !== 0) {
-            return;
-        }
-        $coordinates = [];
-        for ($i = 0; $i < count($polygon); $i = $i + 2) {
-            $coordinates[] = $polygon[$i] . ' ' . $polygon[$i + 1];
-        }
-        $coordinates[] = $coordinates[0];
-        $this->polygon = $coordinates;
-    }
-
-    /**
-     * @return void
-     */
-    public function normalizeProps(): void
-    {
-        parent::normalizeProps();
-        $this->normalizePolygon();
-    }
-
-    /**
-     * @param ActiveQuery $query
-     * @return void
-     */
-    public function setPolygonFilter(ActiveQuery $query): void
-    {
-        if (!$this->polygon) {
-            return;
-        }
-
-        $coords = implode(", ", $this->polygon);
-        $polygonCondition = <<< EOF
-                ST_CONTAINS(
-                ST_GEOMFROMTEXT(
-                    'POLYGON(
-                ($coords)
-                )'
-                        ),
-                        POINT(c_industry_offers_mix.latitude, c_industry_offers_mix.longitude)
-                    )
-            EOF;
-        $query->andWhere(new Expression($polygonCondition));
-    }
-
-    /**
-     * @param ActiveQuery $query
-     * @return void
-     * @throws ValidationErrorHttpException
-     */
-    public function setFilters(ActiveQuery $query): void
-    {
-        parent::setFilters($query);
-        $this->setPolygonFilter($query);
     }
 
     /**
