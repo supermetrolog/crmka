af0e6655edd1f3fc08fd8062e2de47c1f929d556
supermetrolog <billypro6@gmail.com>
2023-06-25 10:56:15 +0300

    normalize objects json fields

--- models/Objects.php
+++ models/Objects.php
@@ -2,7 +2,54 @@
 
 namespace app\models;
 
+use app\helpers\JsonFieldNormalizer;
+
 class Objects extends oldDb\Objects
 {
+    /**
+     * @return array
+     */
+    public function getOwners(): array
+    {
+        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->owners);
+    }
 
+    /**
+     * @return array
+     */
+    public function getObjectType(): array
+    {
+        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->object_type);
+    }
+
+    /**
+     * @return array
+     */
+    public function getFloorBuildings(): array
+    {
+        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->floors_building);
+    }
+
+    /**
+     * @return array
+     */
+    public function getPurposes(): array
+    {
+        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->purposes);
+    }
+
+    /**
+     * @return array
+     */
+    public function fields(): array
+    {
+        $fields = parent::fields();
+
+        $fields['owners'] = function () { return $this->getOwners(); };
+        $fields['object_type'] = function () { return $this->getObjectType(); };
+        $fields['floors_building'] = function () { return $this->getFloorBuildings(); };
+        $fields['purposes'] = function () { return $this->getPurposes(); };
+
+        return $fields;
+    }
 }
