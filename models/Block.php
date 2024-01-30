eccd80a4394d0ae505a3f551c30e9439fea292e1
Тимур Абдурашидов <supermetrolog@Mac-mini-Timur.local>
2024-01-30 23:33:52 +0300

    fix field in block model

--- models/Block.php
+++ models/Block.php
@@ -9,127 +9,178 @@ use yii\helpers\Json;
 class Block extends oldDb\ObjectsBlock
 {
 
-    /**
-     * @return array
-     */
-    public function getPhotos(): array
-    {
-        return Json::decode($this->photo_block) ?? [];
-    }
+	/**
+	 * @return array
+	 */
+	public function getPhotos(): array
+	{
+		return Json::decode($this->photo_block) ?? [];
+	}
 
-    /**
-     * @return array
-     */
-    public function getPurposesBlock(): array
-    {
-        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->purposes_block);
-    }
+	/**
+	 * @return array
+	 */
+	public function getPurposesBlock(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->purposes_block);
+	}
 
-    /**
-     * @return array
-     */
-    public function getFloors(): array
-    {
-        return Json::decode($this->floor) ?? [];
-    }
+	/**
+	 * @return array
+	 */
+	public function getFloors(): array
+	{
+		return Json::decode($this->floor) ?? [];
+	}
 
-    /**
-     * @return array
-     */
-    public function getFloorTypes(): array
-    {
-        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->floor_types);
-    }
+	/**
+	 * @return array
+	 */
+	public function getFloorTypes(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->floor_types);
+	}
 
-    /**
-     * @return array
-     */
-    public function getFirefightingType(): array
-    {
-        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->firefighting_type);
-    }
+	/**
+	 * @return array
+	 */
+	public function getFirefightingType(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->firefighting_type);
+	}
 
-    /**
-     * @return array
-     */
-    public function getVentilation(): array
-    {
-        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->ventilation);
-    }
+	/**
+	 * @return array
+	 */
+	public function getVentilation(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->ventilation);
+	}
 
-    /**
-     * @return array
-     */
-    public function getColumnGrids(): array
-    {
-        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->column_grids);
-    }
+	/**
+	 * @return array
+	 */
+	public function getColumnGrids(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->column_grids);
+	}
 
-    /**
-     * @return array
-     */
-    public function getParts(): array
-    {
-        return JsonFieldNormalizer::jsonToArrayWithIntElements($this->parts);
-    }
+	/**
+	 * @return array
+	 */
+	public function getParts(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->parts);
+	}
 
-    /**
-     * @return array
-     */
-    public function fields(): array
-    {
-        $f = parent::fields();
+	/**
+	 * @return array
+	 */
+	public function getCranesCatHead(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->cranes_cathead);
+	}
 
-        unset($f['photo_block']);
+	/**
+	 * @return array
+	 */
+	public function getCranesOverHead(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->cranes_overhead);
+	}
 
-        $f['photos'] = function () { return $this->getPhotos(); };
-        $f['purposes_block'] = function () { return $this->getPurposesBlock(); };
-        $f['floor'] = function () { return $this->getFloors(); };
-        $f['floor_types'] = function () { return $this->getFloorTypes(); };
-        $f['firefighting_type'] = function () { return $this->getFirefightingType(); };
-        $f['ventilation'] = function () { return $this->getVentilation(); };
-        $f['column_grids'] = function () { return $this->getColumnGrids(); };
-        $f['parts'] = function () { return $this->getParts(); };
+	/**
+	 * @return array
+	 */
+	public function getGates(): array
+	{
+		return JsonFieldNormalizer::jsonToArrayWithIntElements($this->gates);
+	}
 
-        return $f;
-    }
+	/**
+	 * @return array
+	 */
+	public function fields(): array
+	{
+		$f = parent::fields();
 
-    /**
-     * @return array
-     */
-    public function extraFields(): array
-    {
-        $f = parent::extraFields();
+		unset($f['photo_block']);
 
-        $f['floorNumbers'] = 'floorNumbers';
-        $f['partsRecords'] = function () { return $this->getPartsRecords()->asArray()->all(); };
+		$f['photos']            = function () {
+			return $this->getPhotos();
+		};
+		$f['purposes_block']    = function () {
+			return $this->getPurposesBlock();
+		};
+		$f['floor']             = function () {
+			return $this->getFloors();
+		};
+		$f['floor_types']       = function () {
+			return $this->getFloorTypes();
+		};
+		$f['firefighting_type'] = function () {
+			return $this->getFirefightingType();
+		};
+		$f['ventilation']       = function () {
+			return $this->getVentilation();
+		};
+		$f['column_grids']      = function () {
+			return $this->getColumnGrids();
+		};
+		$f['parts']             = function () {
+			return $this->getParts();
+		};
+		$f['cranes_cathead']    = function () {
+			return $this->getCranesCatHead();
+		};
+		$f['cranes_overhead']   = function () {
+			return $this->getCranesOverHead();
+		};
+		$f['gates']             = function () {
+			return $this->getGates();
+		};
 
-        return $f;
-    }
+		return $f;
+	}
 
-    /**
-     * @return array
-     */
-    public function getFloorNumbers(): array
-    {
-        return FloorNumber::find()->andWhere(['sign' => $this->getFloors()])->all();
-    }
+	/**
+	 * @return array
+	 */
+	public function extraFields(): array
+	{
+		$f = parent::extraFields();
 
-    /**
-     * @return ActiveQuery
-     */
-    public function getPartsRecords(): ActiveQuery
-    {
-        return FloorPart::find()
-            ->andWhere(['id' => $this->getParts()])
-            ->with(['floor.number']);
-    }
+		$f['floorNumbers'] = 'floorNumbers';
+		$f['partsRecords'] = function () {
+			return $this->getPartsRecords()->asArray()->all();
+		};
 
-    /**
-     * @return ActiveQuery
-     */
-    public function getDeal(): ActiveQuery
-    {
-        return $this->hasOne(Deal::class, ['original_id' => 'id']);
-    }
+		return $f;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getFloorNumbers(): array
+	{
+		return FloorNumber::find()->andWhere(['sign' => $this->getFloors()])->all();
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getPartsRecords(): ActiveQuery
+	{
+		return FloorPart::find()
+		                ->andWhere(['id' => $this->getParts()])
+		                ->with(['floor.number']);
+	}
+
+	/**
+	 * @return ActiveQuery
+	 */
+	public function getDeal(): ActiveQuery
+	{
+		return $this->hasOne(Deal::class, ['original_id' => 'id']);
+	}
 }
