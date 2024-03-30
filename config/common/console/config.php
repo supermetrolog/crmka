7a764419613abb3b5f4641bc8a02c7fe879c2b9c
Тимур Абдурашидов <supermetrolog@Mac-mini-Timur.local>
2024-03-30 21:03:19 +0300

    change migration views

--- config/common/console/config.php
+++ config/common/console/config.php
@@ -3,13 +3,26 @@
 use yii\helpers\ArrayHelper;
 
 return ArrayHelper::merge(
-    require __DIR__ . '/../common/config.php',
-    [
-        'id' => 'basic-console',
-        'bootstrap' => ['queue'],
-        'aliases' => [
-            '@tests' => '@app/tests'
-        ],
-        'controllerNamespace' => 'app\commands',
-    ]
+	require __DIR__ . '/../common/config.php',
+	[
+		'id'                  => 'basic-console',
+		'bootstrap'           => ['queue'],
+		'aliases'             => [
+			'@tests' => '@app/tests'
+		],
+		'controllerNamespace' => 'app\commands',
+		'controllerMap'       => [
+			'migrate' => [
+				'class'                  => 'yii\console\controllers\MigrateController',
+				'templateFile'           => '@app/kernel/console/views/migration.php',
+				'generatorTemplateFiles' => [
+					'create_table'    => '@app/kernel/console/views/createTableMigration.php',
+					'drop_table'      => '@yii/views/dropTableMigration.php',
+					'add_column'      => '@yii/views/addColumnMigration.php',
+					'drop_column'     => '@yii/views/dropColumnMigration.php',
+					'create_junction' => '@yii/views/createTableMigration.php',
+				]
+			],
+		],
+	]
 );
