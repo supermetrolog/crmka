d734278cb0cf9fbd34fa44f43c7a80937fc8bb54
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-05-25 12:14:59 +0300

    Add models

---
+++ models/ActiveQuery/MediaQuery.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\models\ActiveQuery;
+
+use app\kernel\common\models\AQ\SoftDeleteTrait;
+use app\kernel\common\models\exceptions\ModelNotFoundException;
+use app\models\Media;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the ActiveQuery class for [[\app\models\Media]].
+ *
+ * @see Media
+ */
+class MediaQuery extends ActiveQuery
+{
+	use SoftDeleteTrait;
+
+	/**
+	 * @return Media[]|ActiveRecord[]
+	 */
+	public function all($db = null): array
+	{
+		return parent::all($db);
+	}
+
+	/**
+	 * @return Media|ActiveRecord|null
+	 */
+	public function one($db = null): ?Media
+	{
+		return parent::one($db);
+	}
+
+	/**
+	 * @return Media|ActiveRecord|null
+	 * @throws ModelNotFoundException
+	 */
+	public function oneOrThrow($db = null): Media
+	{
+		return parent::oneOrThrow($db);
+	}
+
+	public function byCreatedById(int $id): self
+	{
+		return $this->andWhere([$this->field('created_by_id') => $id]);
+	}
+
+	public function byCreatedByType(string $type): self
+	{
+		return $this->andWhere([$this->field('created_by_type') => $type]);
+	}
+
+	public function byMorph(int $id, string $type): self
+	{
+		return $this->byCreatedByType($type)->byCreatedById($id);
+	}
+}
