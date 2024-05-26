61aa5155163c35d26730d115a69993514f91c7a5
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-05-26 03:07:36 +0300

    Update CreateMediaService.php

--- usecases/Media/CreateMediaService.php
+++ usecases/Media/CreateMediaService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace app\usecases\Media;
 
 use app\dto\Media\CreateMediaDto;
+use app\kernel\common\database\interfaces\transaction\TransactionBeginnerInterface;
 use app\kernel\common\models\exceptions\SaveModelException;
 use app\models\Media;
 use app\components\Media as MediaComponent;
@@ -13,33 +14,47 @@ use yii\web\UploadedFile;
 class CreateMediaService
 {
 	private MediaComponent $media;
+	private TransactionBeginnerInterface $transactionBeginner;
 
-	public function __construct(MediaComponent $media)
+	public function __construct(
+		MediaComponent $media,
+		TransactionBeginnerInterface $transactionBeginner
+	)
 	{
 		$this->media = $media;
+		$this->transactionBeginner = $transactionBeginner;
 	}
 
 	/**
-	 * @throws SaveModelException
+	 * @throws \Throwable
 	 */
 	public function create(CreateMediaDto $dto): Media
 	{
-		$name = md5($dto->uploadedFile->name . time()) . $dto->uploadedFile->extension;
+		$tx = $this->transactionBeginner->begin();
 
-		$this->media->put($name, $dto->uploadedFile);
+		try {
+			$name = md5($dto->uploadedFile->name . time()) . $dto->uploadedFile->extension;
 
-		$media = new Media([
-			'name'          => $name,
-			'original_name' => $dto->uploadedFile->name,
-			'extension'     => $dto->uploadedFile->extension,
-			'path'          => $this->media->webPath($name),
-			'category'      => $dto->category,
-			'model_type'    => $dto->model_type,
-			'model_id'      => $dto->model_id,
-		]);
+			$this->media->put($name, $dto->uploadedFile);
 
-		$media->saveOrThrow();
+			$media = new Media([
+				'name'          => $name,
+				'original_name' => $dto->uploadedFile->name,
+				'extension'     => $dto->uploadedFile->extension,
+				'path'          => $this->media->webPath($name),
+				'category'      => $dto->category,
+				'model_type'    => $dto->model_type,
+				'model_id'      => $dto->model_id,
+			]);
 
-		return $media;
+			$media->saveOrThrow();
+
+			$tx->commit();
+
+			return $media;
+		} catch (\Throwable $th) {
+			$tx->rollback();
+			throw $th;
+		}
 	}
 }
