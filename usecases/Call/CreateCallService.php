509b32c3c029d66c8ec75e1a9fb6216bbb9f4055
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-05 19:35:17 +0300

    Create services

---
+++ usecases/Call/CreateCallService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\usecases\Call;
+
+use app\dto\Call\CreateCallDto;
+use app\kernel\common\models\exceptions\SaveModelException;
+use app\models\Call;
+
+class CreateCallService
+{
+	/**
+	 * @throws SaveModelException
+	 */
+	public function create(CreateCallDto $dto): Call
+	{
+		$model = new Call([
+			'user_id'    => $dto->user->id,
+			'contact_id' => !empty($dto->contact) ? $dto->contact->id : null,
+		]);
+
+		$model->saveOrThrow();
+
+		return $model;
+	}
+}
