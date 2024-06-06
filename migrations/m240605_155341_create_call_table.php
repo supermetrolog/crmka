c24002a48dc9b00043726454cd295c4f40214c26
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-07 00:39:07 +0300

    Update m240605_155341_create_call_table.php

--- migrations/m240605_155341_create_call_table.php
+++ migrations/m240605_155341_create_call_table.php
@@ -7,42 +7,45 @@ use app\kernel\console\Migration;
  */
 class m240605_155341_create_call_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp()
+	{
 		$tableName = '{{%call}}';
 		$this->table($tableName, [
-            'id' => $this->primaryKey(),
-            'user_id'     => $this->integer()->notNull(),
-            'contact_id'  => $this->integer()->null(),
-        ], $this->timestamps(), $this->softDelete());
+			'id'         => $this->primaryKey(),
+			'user_id'    => $this->integer()->notNull(),
+			'contact_id' => $this->integer()->null(),
+		], $this->timestamps(), $this->softDelete());
 
-	    $this->index($tableName, ['user_id']);
-	    $this->index($tableName, ['contact_id']);
+		$this->index($tableName, ['user_id']);
+		$this->index($tableName, ['contact_id']);
 
-	    $this->foreignKey(
-		    $tableName,
-		    ['user_id'],
-		    'user',
-		    ['id']
-	    );
+		$this->foreignKey(
+			$tableName,
+			['user_id'],
+			'user',
+			['id']
+		);
 
-	    $this->foreignKey(
-		    $tableName,
-		    ['contact_id'],
-		    'contact',
-		    ['id']
-	    );
-    }
+		$this->foreignKey(
+			$tableName,
+			['contact_id'],
+			'contact',
+			['id']
+		);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown()
+	{
 		$tableName = '{{%call}}';
-        $this->dropTable($tableName);
-    }
+
+		$this->foreignKeyDrop($tableName, ['user_id']);
+		$this->foreignKeyDrop($tableName, ['contact_id']);
+		$this->dropTable($tableName);
+	}
 }
