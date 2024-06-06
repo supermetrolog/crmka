6603977f071cb55ae42539e70d82456084911563
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-07 00:42:27 +0300

    Update CallSearch.php

--- models/search/CallSearch.php
+++ models/search/CallSearch.php
@@ -15,39 +15,39 @@ class CallSearch extends Form
 	public $created_at;
 	public $updated_at;
 	public $deleted_at;
-	
-    public function rules(): array
-    {
-        return [
-            [['id', 'user_id', 'contact_id'], 'integer'],
-            [['created_at', 'updated_at', 'deleted_at'], 'safe'],
-        ];
-    }
+
+	public function rules(): array
+	{
+		return [
+			[['id', 'user_id', 'contact_id'], 'integer'],
+			[['created_at', 'updated_at', 'deleted_at'], 'safe'],
+		];
+	}
 
 	/**
 	 * @throws ValidateException
 	 */
-    public function search(array $params): ActiveDataProvider
-    {
-        $query = Call::find();
+	public function search(array $params): ActiveDataProvider
+	{
+		$query = Call::find();
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
 
-        $this->load($params);
+		$this->load($params);
 
 		$this->validateOrThrow();
 
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'user_id' => $this->user_id,
-            'contact_id' => $this->contact_id,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'deleted_at' => $this->deleted_at,
-        ]);
+		$query->andFilterWhere([
+			'id'         => $this->id,
+			'user_id'    => $this->user_id,
+			'contact_id' => $this->contact_id,
+			'created_at' => $this->created_at,
+			'updated_at' => $this->updated_at,
+			'deleted_at' => $this->deleted_at,
+		]);
 
-        return $dataProvider;
-    }
+		return $dataProvider;
+	}
 }
