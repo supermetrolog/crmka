7c7fd553acc53cdb35fe46a9cc1bb2e7834b0c26
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-07 23:45:17 +0300

    Create m240607_204221_create_question_answer_table.php

---
+++ migrations/m240607_204221_create_question_answer_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use app\kernel\console\Migration;
+
+/**
+ * Handles the creation of table `{{%question_answer}}`.
+ */
+class m240607_204221_create_question_answer_table extends Migration
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp()
+	{
+		$tableName = '{{%question_answer}}';
+		$this->table($tableName, [
+			'id'       => $this->primaryKey(),
+			'field_id' => $this->integer()->notNull(),
+			'category' => $this->tinyInteger()->notNull(),
+			'value'    => $this->string()->notNull(),
+		], $this->timestamps(), $this->softDelete());
+
+		$this->index($tableName, ['field_id']);
+
+		$this->foreignKey($tableName, ['field_id'], 'field', ['id']);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown()
+	{
+		$tableName = '{{%question_answer}}';
+		
+		$this->foreignKeyDrop($tableName, ['field_id']);
+		$this->dropTable($tableName);
+	}
+}
