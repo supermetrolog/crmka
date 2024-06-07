eba0fc097b270d274727d442980fbef96a264c8c
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-07 20:28:42 +0300

    Write Survey form

---
+++ dto/Survey/CreateSurveyDto.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\dto\Survey;
+
+use app\models\Contact;
+use app\models\User;
+use yii\base\BaseObject;
+
+class CreateSurveyDto extends BaseObject
+{
+	public User    $user;
+	public Contact $contact;
+}
