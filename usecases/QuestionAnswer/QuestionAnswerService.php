15aa7090d57d6ff58f4be6729c7e1b7c30cd1a40
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-08 00:06:11 +0300

    Create QuestionAnswerService.php

---
+++ usecases/QuestionAnswer/QuestionAnswerService.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\usecases\QuestionAnswer;
+
+use app\dto\QuestionAnswer\CreateQuestionAnswerDto;
+use app\dto\QuestionAnswer\UpdateQuestionAnswerDto;
+use app\kernel\common\models\exceptions\SaveModelException;
+use app\models\QuestionAnswer;
+use Throwable;
+use yii\db\StaleObjectException;
+
+class QuestionAnswerService
+{
+	/**
+	 * @throws SaveModelException
+	 */
+	public function create(CreateQuestionAnswerDto $dto): QuestionAnswer
+	{
+		$model = new QuestionAnswer([
+			'field_id' => $dto->field->id,
+			'category' => $dto->category,
+			'value'    => $dto->value,
+		]);
+
+		$model->saveOrThrow();
+
+		return $model;
+	}
+
+	/**
+	 * @throws SaveModelException
+	 */
+	public function update(QuestionAnswer $model, UpdateQuestionAnswerDto $dto): QuestionAnswer
+	{
+		$model->load([
+			'field_id' => $dto->field->id,
+			'category' => $dto->category,
+			'value'    => $dto->value,
+		]);
+
+		$model->saveOrThrow();
+
+		return $model;
+	}
+
+	/**
+	 * @throws StaleObjectException
+	 * @throws Throwable
+	 */
+	public function delete(QuestionAnswer $model): void
+	{
+		$model->delete();
+	}
+}
