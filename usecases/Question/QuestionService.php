1e2dcf4d7b4ca04795337e0c9a433eff3d157c43
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-07 22:05:01 +0300

    Create QuestionService.php

---
+++ usecases/Question/QuestionService.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\usecases\Question;
+
+use app\dto\Question\CreateQuestionDto;
+use app\dto\Question\UpdateQuestionDto;
+use app\kernel\common\models\exceptions\SaveModelException;
+use app\models\Question;
+use Throwable;
+use yii\db\StaleObjectException;
+
+class QuestionService
+{
+	/**
+	 * @throws SaveModelException
+	 */
+	public function create(CreateQuestionDto $dto): Question
+	{
+		$model = new Question([
+			'text' => $dto->text,
+		]);
+
+		$model->saveOrThrow();
+
+		return $model;
+	}
+
+	/**
+	 * @throws SaveModelException
+	 */
+	public function update(Question $model, UpdateQuestionDto $dto): Question
+	{
+		$model->load([
+			'user_id' => $dto->text,
+		]);
+
+		$model->saveOrThrow();
+
+		return $model;
+	}
+
+	/**
+	 * @throws StaleObjectException
+	 * @throws Throwable
+	 */
+	public function delete(Question $model): void
+	{
+		$model->delete();
+	}
+}
