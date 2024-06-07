b4e2dc4085c8f56b4b1cbace12c6e67c528293a2
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-08 00:29:04 +0300

    Write SurveyQuestionAnswer Form

---
+++ dto/SurveyQuestionAnswer/UpdateSurveyQuestionAnswerDto.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\dto\SurveyQuestionAnswer;
+
+use app\models\QuestionAnswer;
+use app\models\Survey;
+use yii\base\BaseObject;
+
+class UpdateSurveyQuestionAnswerDto extends BaseObject
+{
+	public QuestionAnswer $question_answer;
+	public Survey         $survey;
+	public ?string        $value;
+}
