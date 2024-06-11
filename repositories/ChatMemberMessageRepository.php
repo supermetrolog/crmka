371d700d8ce8b0408089687a2ea25e7850c3dd2b
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-11 22:33:14 +0300

    Fix comments

--- repositories/ChatMemberMessageRepository.php
+++ repositories/ChatMemberMessageRepository.php
@@ -7,7 +7,7 @@ namespace app\repositories;
 use app\models\ChatMemberMessage;
 use app\models\ChatMemberMessageView;
 use yii\db\BatchQueryResult;
-use yii\db\conditions\OrCondition;
+use yii\db\Expression;
 
 class ChatMemberMessageRepository
 {
@@ -17,14 +17,14 @@ class ChatMemberMessageRepository
 	public function findPreviousUnreadByMessage(ChatMemberMessage $message): BatchQueryResult
 	{
 		return ChatMemberMessage::find()
-								->with('notifications')
-		                        ->leftJoin(ChatMemberMessageView::getTable(), ChatMemberMessageView::getColumn('chat_member_message_id') . '=' . ChatMemberMessage::getColumn('id'))
-		                        ->where(['<=', ChatMemberMessage::getColumn('id'), $message->id])
+		                        ->with('notifications')
+		                        ->joinWith('views')
+		                        ->andWhere(['<=', ChatMemberMessage::getColumn('id'), $message->id])
 		                        ->andWhere([ChatMemberMessage::getColumn('to_chat_member_id') => $message->to_chat_member_id])
-		                        ->andWhere(new OrCondition([
-			                        [ChatMemberMessageView::getColumn('id') => null],
-			                        ChatMemberMessageView::getColumn('chat_member_id') . '!=' . ChatMemberMessage::getColumn('from_chat_member_id'),
-		                        ]))
+		                        ->andWhere(['OR',
+		                                    ['IS', ChatMemberMessageView::getColumn('id'), null],
+		                                    ['!=', ChatMemberMessageView::getColumn('chat_member_id'), new Expression(ChatMemberMessage::getColumn('from_chat_member_id'))],
+		                        ])
 		                        ->notDeleted()
 		                        ->each();
 	}
