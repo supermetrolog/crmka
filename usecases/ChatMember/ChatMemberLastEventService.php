7d4cbe66fd5c071d8631f786f1af6ba79e593929
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-06-17 21:35:50 +0300

    Add ChatMemberLastEventService

---
+++ usecases/ChatMember/ChatMemberLastEventService.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\usecases\ChatMember;
+
+use app\dto\ChatMember\CreateChatMemberLastEventDto;
+use app\kernel\common\models\exceptions\SaveModelException;
+use app\models\ChatMemberLastEvent;
+use Throwable;
+
+class ChatMemberLastEventService
+{
+	/**
+	 * @throws SaveModelException
+	 */
+	public function create(CreateChatMemberLastEventDto $dto): ChatMemberLastEvent
+	{
+		$model = new ChatMemberLastEvent([
+			'chat_member_id' => $dto->chat_member_id,
+			'event_chat_member_id' => $dto->event_chat_member_id,
+		]);
+
+		$model->saveOrThrow();
+
+		return $model;
+	}
+	/**
+	 * @throws SaveModelException
+	 */
+	public function update(ChatMemberLastEvent $model): ChatMemberLastEvent
+	{
+		$model->saveOrThrow();
+
+		return $model;
+	}
+}
