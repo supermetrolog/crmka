8e0dc5ad5e14a6c89d5cfceccd2877daae0414bd
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-07-02 01:20:55 +0300

    Add leftJoinLastCallRelation

--- models/ActiveQuery/EquipmentQuery.php
+++ models/ActiveQuery/EquipmentQuery.php
@@ -4,7 +4,9 @@ namespace app\models\ActiveQuery;
 
 use app\kernel\common\models\AQ\AQ;
 use app\kernel\common\models\exceptions\ModelNotFoundException;
+use app\models\Call;
 use app\models\Equipment;
+use app\models\Relation;
 use yii\db\ActiveRecord;
 
 /**
@@ -38,5 +40,39 @@ class EquipmentQuery extends AQ
 	public function oneOrThrow($db = null): Equipment
 	{
 		return parent::oneOrThrow($db);
+	}
+
+
+	public function byModelId(int $id): self
+	{
+		return $this->andWhere([$this->field('model_id') => $id]);
+	}
+
+	public function byModelType(string $type): self
+	{
+		return $this->andWhere([$this->field('model_type') => $type]);
+	}
+
+	public function byMorph(int $id, string $type): self
+	{
+		return $this->byModelType($type)->byModelId($id);
+	}
+
+	public function leftJoinLastCallRelation(): self
+	{
+		$maxIdsSubQuery = Relation::find()
+		                          ->select(['MAX(id)'])
+		                          ->byFirstType(Equipment::getMorphClass())
+		                          ->bySecondType(Call::getMorphClass())
+		                          ->groupBy(['first_id', 'first_type']);
+
+		$subQuery = Relation::find()
+		                    ->byFirstType(Equipment::getMorphClass())
+		                    ->bySecondType(Call::getMorphClass())
+		                    ->andWhere(['id' => $maxIdsSubQuery]);
+
+		$this->leftJoin(['last_call_rel' => $subQuery], $this->field('id') . '=' . 'last_call_rel.first_id');
+
+		return $this;
 	}
 }
