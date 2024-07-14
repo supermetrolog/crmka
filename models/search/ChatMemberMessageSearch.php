f25fe03b7fdc6fec10cc3c2b50f38d025880dd3e
Petya <58220335+StulE-ru@users.noreply.github.com>
2024-07-14 12:20:09 +0300

    Update ChatMemberMessageSearch.php

--- models/search/ChatMemberMessageSearch.php
+++ models/search/ChatMemberMessageSearch.php
@@ -2,6 +2,7 @@
 
 namespace app\models\search;
 
+use app\kernel\common\models\AR\AR;
 use app\kernel\common\models\exceptions\ValidateException;
 use app\kernel\common\models\Form\Form;
 use app\models\ChatMemberMessage;
@@ -14,7 +15,6 @@ class ChatMemberMessageSearch extends Form
 {
 	public $id;
 	public $to_chat_member_id;
-	public $from_chat_member_id;
 	public $message;
 	public $created_at;
 	public $updated_at;
@@ -26,7 +26,7 @@ class ChatMemberMessageSearch extends Form
 	public function rules(): array
 	{
 		return [
-			[['id', 'to_chat_member_id', 'from_chat_member_id', 'id_less_then'], 'integer'],
+			[['id', 'to_chat_member_id', 'id_less_then'], 'integer'],
 			[['message', 'created_at', 'updated_at'], 'safe'],
 		];
 	}
@@ -36,6 +36,16 @@ class ChatMemberMessageSearch extends Form
 	 */
 	public function search(array $params): ActiveDataProvider
 	{
+		$unreadCount = ChatMemberMessage::find()
+		                                ->select([
+			                                'count' => 'COUNT(' . ChatMemberMessage::field('id') . ')',
+		                                ])
+		                                ->joinWith('views')
+		                                ->byFromChatMemberId($this->current_from_chat_member_id)
+		                                ->andWhereNull(ChatMemberMessageView::getColumn('id'))
+		                                ->notDeleted()
+		                                ->asArray()->all()[0]['count'];
+
 		$query = ChatMemberMessage::find()
 		                          ->select([
 			                          ChatMemberMessage::field('*'),
@@ -43,6 +53,7 @@ class ChatMemberMessageSearch extends Form
 		                          ])
 		                          ->joinWith('fromChatMemberViews')
 		                          ->andWhere([ChatMemberMessage::field('from_chat_member_id') => $this->current_from_chat_member_id])
+		                          ->byFromChatMemberId($this->current_from_chat_member_id)
 		                          ->with(['fromChatMember.objectChatMember', 'fromChatMember.request'])
 		                          ->with(['fromChatMember.user.userProfile'])
 		                          ->with(['tasks.createdByUser.userProfile'])
@@ -64,26 +75,30 @@ class ChatMemberMessageSearch extends Form
 
 		$this->validateOrThrow();
 
-		if ($this->id_less_then === null) {
+		if ($this->id_less_then === null && 0 < $unreadCount) {
 			$query->joinWith('views')
-			      ->andWhere([
-					  ChatMemberMessage::field('from_chat_member_id') => $this->current_from_chat_member_id,
-					  ChatMemberMessageView::field('id') => null,
-			      ]);
+			      ->orderBy([
+				      '(CASE WHEN ' . ChatMemberMessageView::field('id') . ' IS NULL THEN 0 ELSE 1 END)' => SORT_ASC,
+				      ChatMemberMessage::field('id')                                                     => SORT_DESC,
+			      ])
+			      ->limit($unreadCount < 30 ? 30 : $unreadCount + 5);
+
+			$orderedQuery = ChatMemberMessage::find()->from(['messages' => $query])->orderBy(['messages.id' => SORT_ASC]);
+
+			$dataProvider->pagination = false;
+			$dataProvider->query      = $orderedQuery;
 		}
 
 		$query->andFilterWhere([
-			ChatMemberMessage::field('id')                  => $this->id,
-			ChatMemberMessage::field('to_chat_member_id')   => $this->to_chat_member_id,
-			ChatMemberMessage::field('from_chat_member_id') => $this->from_chat_member_id,
-			ChatMemberMessage::field('created_at')          => $this->created_at,
-			ChatMemberMessage::field('updated_at')          => $this->updated_at,
+			ChatMemberMessage::field('id')                => $this->id,
+			ChatMemberMessage::field('to_chat_member_id') => $this->to_chat_member_id,
+			ChatMemberMessage::field('created_at')        => $this->created_at,
+			ChatMemberMessage::field('updated_at')        => $this->updated_at,
 		]);
 
 		$query->andFilterWhere(['<', ChatMemberMessage::field('id'), $this->id_less_then]);
 
 		$query->andFilterWhere(['like', ChatMemberMessage::field('message'), $this->message]);
-
 
 		return $dataProvider;
 	}
