7a3bd90fc30641871f3b1b293d891d00d2f66164
nknwns <andrejshaev@gmail.com>
2024-09-11 11:37:35 +0700

    feat(IfExpressionBuilder): add `as(string $alias)` method - you can set alias after condition

--- components/ExpressionBuilder/IfExpressionBuilder.php
+++ components/ExpressionBuilder/IfExpressionBuilder.php
@@ -8,6 +8,19 @@ use yii\db\Expression;
 /**
  * Построитель выражения `IF` для SQL запросов с использованием `yii\db\Expression`
  *
+ * Пример использования:
+ *
+ * ```php
+ * $expression = IfExpressionBuilder::create()
+ *         ->condition('object.count > 10')
+ *         ->left(1)
+ *         ->right(0)
+ *         ->as('is_big')
+ *         ->build();
+ *  // equals to:
+ *  // $expression = new Expression("IF(object.count > 10, 1, 0) AS is_big");
+ * ```
+ *
  * @package app\components\ExpressionBuilder
  */
 class IfExpressionBuilder
@@ -15,6 +28,7 @@ class IfExpressionBuilder
 	private           $condition;
 	private           $trueExpression;
 	private           $falseExpression;
+	private ?string   $alias = null;
 	protected Closure $transformFn;
 
 	public function __construct($condition = null, $trueExpression = 1, $falseExpression = 0)
@@ -80,6 +94,20 @@ class IfExpressionBuilder
 	}
 
 	/**
+	 * Устанавливает псевдоним для выражения
+	 *
+	 * @param string $alias
+	 *
+	 * @return $this
+	 */
+	public function as(string $alias): self
+	{
+		$this->alias = $alias;
+
+		return $this;
+	}
+
+	/**
 	 * Устанавливает функцию для обработки выражения перед созданием объекта `Expression`
 	 *
 	 * @param callable $transformFn
@@ -111,8 +139,13 @@ class IfExpressionBuilder
 	public function toString(): string
 	{
 		$expression = "IF($this->condition, $this->trueExpression, $this->falseExpression)";
+		$string     = ($this->transformFn)($expression);
 
-		return ($this->transformFn)($expression);
+		if (!is_null($this->alias)) {
+			$string .= " AS $this->alias";
+		}
+
+		return $string;
 	}
 
 	/**
