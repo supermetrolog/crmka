c9396b6be78dfef798045fc92f338680c7bb7b53
nknwns <andrejshaev@gmail.com>
2024-10-02 02:57:51 +0700

    chore: create helpers for DateInterval and Token

---
+++ helpers/TokenHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\helpers;
+
+/**
+ * Helper for working with tokens
+ *
+ * @package app\helpers
+ */
+class TokenHelper
+{
+	/** Parse Bearer token from Authorization header
+	 *
+	 * @param string $header Authorization header value
+	 *
+	 * @return string|null Bearer token or null if not found
+	 */
+	public static function parseBearerToken(string $header): ?string
+	{
+		$matches = [];
+		preg_match('/Bearer\s(\S+)/', $header, $matches);
+
+		return $matches[1] ?? null;
+	}
+}
