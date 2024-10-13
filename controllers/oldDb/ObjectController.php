411a95628ec4e59c2edffea83242dd9a11464aa8
nknwns <andrejshaev@gmail.com>
2024-10-13 19:31:56 +0700

    refactor: create OfferMixService, improve ObjectController using AppController

--- controllers/oldDb/ObjectController.php
+++ controllers/oldDb/ObjectController.php
@@ -2,58 +2,34 @@
 
 namespace app\controllers\oldDb;
 
-use app\behaviors\BaseControllerBehaviors;
 use app\exceptions\ValidationErrorHttpException;
-use app\models\Company;
+use app\kernel\common\controller\AppController;
+use app\kernel\common\models\exceptions\ModelNotFoundException;
+use app\kernel\common\models\exceptions\SaveModelException;
+use app\kernel\web\http\responses\SuccessResponse;
 use app\models\OfferMix;
 use app\models\oldDb\ObjectsSearch;
 use app\models\oldDb\OfferMixMapSearch;
 use app\models\oldDb\OfferMixSearch;
-use Yii;
+use app\usecases\OfferMix\OfferMixService;
+use Throwable;
 use yii\base\ErrorException;
 use yii\data\ActiveDataProvider;
-use yii\filters\Cors;
-use yii\rest\ActiveController;
-use yii\web\NotFoundHttpException;
-use yii\web\Response;
 
-class ObjectController extends ActiveController
+class ObjectController extends AppController
 {
-	public $modelClass = Company::class;
+	private OfferMixService $offerMixService;
 
-	/**
-	 * @return array
-	 */
-	public function behaviors(): array
+	public function __construct(
+		$id,
+		$module,
+		OfferMixService $offerMixService,
+		array $config = []
+	)
 	{
-		$behaviors               = parent::behaviors();
-		$behaviors               = BaseControllerBehaviors::getBaseBehaviors($behaviors, ['index', 'view']);
-		$behaviors['corsFilter'] = [
-			'class' => Cors::class,
-			'cors'  => [
-				'Origin'                         => ['*'],
-				'Access-Control-Request-Method'  => ['*'],
-				'Access-Control-Request-Headers' => ['Origin', 'Content-Type', 'Accept', 'Authorization'],
-				'Access-Control-Expose-Headers'  => ['X-Pagination-Total-Count', 'X-Pagination-Page-Count', 'X-Pagination-Current-Page', 'X-Pagination-Per-Page', 'Link']
-			],
-		];
+		$this->offerMixService = $offerMixService;
 
-		return $behaviors;
-	}
-
-	/**
-	 * @return array
-	 */
-	public function actions(): array
-	{
-		$actions = parent::actions();
-		unset($actions['index']);
-		unset($actions['view']);
-		unset($actions['create']);
-		unset($actions['update']);
-		unset($actions['delete']);
-
-		return $actions;
+		parent::__construct($id, $module, $config);
 	}
 
 	/**
@@ -72,13 +48,7 @@ class ObjectController extends ActiveController
 	 */
 	public function actionOffers(): ActiveDataProvider
 	{
-		$this->response->format = Response::FORMAT_JSON;
-		$this->response->on(Response::EVENT_BEFORE_SEND, function () {
-			$this->response->headers->remove('link');
-		});
-
-		// TODO: Change to $this->user when controller is switched to rest controller
-		$identity = Yii::$app->user->identity;
+		$identity = $this->user->identity;
 
 		$searchModel = new OfferMixSearch();
 
@@ -93,11 +63,6 @@ class ObjectController extends ActiveController
 	 */
 	public function actionOffersCount()
 	{
-		$this->response->format = Response::FORMAT_JSON;
-		$this->response->on(Response::EVENT_BEFORE_SEND, function () {
-			$this->response->headers->remove('link');
-		});
-
 		$searchModel = new OfferMixSearch();
 
 		$dataProvider = $searchModel->search($this->request->get());
@@ -111,11 +76,6 @@ class ObjectController extends ActiveController
 	 */
 	public function actionOffersMapCount()
 	{
-		$this->response->format = Response::FORMAT_JSON;
-		$this->response->on(Response::EVENT_BEFORE_SEND, function () {
-			$this->response->headers->remove('link');
-		});
-
 		$searchModel = new OfferMixMapSearch();
 
 		$dataProvider = $searchModel->search($this->request->get());
@@ -129,10 +89,6 @@ class ObjectController extends ActiveController
 	 */
 	public function actionOffersMap(): ActiveDataProvider
 	{
-		$this->response->on(Response::EVENT_BEFORE_SEND, function () {
-			$this->response->headers->remove('link');
-		});
-
 		$searchModel = new OfferMixMapSearch();
 
 		return $searchModel->search($this->request->get());
@@ -141,63 +97,34 @@ class ObjectController extends ActiveController
 	/**
 	 * @param int $originalId
 	 *
-	 * @return void
-	 * @throws NotFoundHttpException|ErrorException
+	 * @return SuccessResponse
+	 * @throws ModelNotFoundException
+	 * @throws SaveModelException
+	 * @throws Throwable
 	 */
-	// TODO: Move to service
-	public function actionToggleAvitoAd(int $originalId): void
+	public function actionToggleAvitoAd(int $originalId): SuccessResponse
 	{
-		$model = OfferMix::find()->byOriginalId($originalId)->one();
+		$model = OfferMix::find()->byOriginalId($originalId)->oneOrThrow();
 
-		if (!$model) {
-			throw new NotFoundHttpException('Offer not found');
-		}
+		$avitoAdIsActive = $this->offerMixService->toggleAvitoAd($model);
 
-		$block = $model->block;
-
-		if (!$block) {
-			throw new NotFoundHttpException('Offer block not found');
-		}
-
-		$block->ad_avito = !$block->ad_avito;
-		$model->ad_avito = $block->ad_avito;
-
-		if ($block->ad_avito) {
-			$block->ad_avito_date_start = date('Y-m-d H:i:s');
-		} else {
-			$block->ad_avito_date_start = null;
-		}
-
-		$model->save(false);
-		$block->save(false);
+		return new SuccessResponse('Реклама на Авито успешно ' . $avitoAdIsActive ? 'подключена' : 'отключена');
 	}
 
 	/**
 	 * @param int $originalId
 	 *
-	 * @return void
-	 * @throws NotFoundHttpException|ErrorException
+	 * @return SuccessResponse
+	 * @throws ModelNotFoundException
+	 * @throws SaveModelException
+	 * @throws Throwable
 	 */
-
-	// TODO: Move to service
-	public function actionToggleIsFake(int $originalId): void
+	public function actionToggleIsFake(int $originalId): SuccessResponse
 	{
-		$model = OfferMix::find()->byOriginalId($originalId)->one();
+		$model = OfferMix::find()->byOriginalId($originalId)->oneOrThrow();
 
-		if (!$model) {
-			throw new NotFoundHttpException('Offer not found');
-		}
+		$offerIsFake = $this->offerMixService->toggleIsFake($model);
 
-		$block = $model->block;
-
-		if (!$block) {
-			throw new NotFoundHttpException('Offer block not found');
-		}
-
-		$block->is_fake = !$block->is_fake;
-		$model->is_fake = $block->is_fake;
-
-		$model->save(false);
-		$block->save(false);
+		return new SuccessResponse($offerIsFake ? 'Предложение помечено как фейковое' : 'Предложение больше не является фейкомым');
 	}
 }
