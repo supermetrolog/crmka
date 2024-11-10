00fde10b0da1d5d75b8620e93d9e6e17d7650221
nknwns <andrejshaev@gmail.com>
2024-11-11 00:40:18 +0700

    fix: delete useless json decoding

--- listeners/Survey/QuestionAnswerEffectListener.php
+++ listeners/Survey/QuestionAnswerEffectListener.php
@@ -43,7 +43,7 @@ class QuestionAnswerEffectListener implements EventListenerInterface
 
 		foreach ($questionAnswers as $answer) {
 			if ($answer->hasEffectByKind(EffectKind::REQUESTS_NO_LONGER_RELEVANT)) {
-				$eventShouldBeTriggered = (bool)json_decode($answer->surveyQuestionAnswer->value);
+				$eventShouldBeTriggered = $answer->surveyQuestionAnswer->value;
 
 				if ($eventShouldBeTriggered) {
 					$event = new SurveyRequestsNoLongerRelevantEvent($survey->id);
@@ -52,7 +52,7 @@ class QuestionAnswerEffectListener implements EventListenerInterface
 			}
 
 			if ($answer->hasEffectByKind(EffectKind::COMPANY_PLANNED_DEVELOP)) {
-				$eventShouldBeTriggered = (bool)json_decode($answer->surveyQuestionAnswer->value);
+				$eventShouldBeTriggered = $answer->surveyQuestionAnswer->value;
 
 				if ($eventShouldBeTriggered) {
 					$event = new SurveyCompanyPlannedDevelopEvent($survey->id);
