4b2505efe9b29581be6001e38bab39f6ebeda2b2
nknwns <andrejshaev@gmail.com>
2024-12-06 01:23:42 +0700

    fix: fix task search query

--- models/search/TaskSearch.php
+++ models/search/TaskSearch.php
@@ -52,6 +52,7 @@ class TaskSearch extends Form
 		$query = Task::find()
 		             ->with(['user.userProfile', 'createdByUser.userProfile', 'tags', 'observers.user.userProfile'])
 		             ->joinWith(['tags', 'targetUserObserver tuo'])
+		             ->groupBy(Task::field('id'))
 		             ->notDeleted();
 
 		$dataProvider = new ActiveDataProvider([
