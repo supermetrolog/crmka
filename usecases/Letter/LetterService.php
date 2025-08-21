28f61665aa87b4252a9d876143f62b0a22d0ce3b
nknwns <andrejshaev@gmail.com>
2025-08-21 13:54:05 +0700

    debug smtp

--- usecases/Letter/LetterService.php
+++ usecases/Letter/LetterService.php
@@ -56,16 +56,16 @@ class LetterService
 				return $createLetterModel->letterModel;
 			}
 
-			$this->queue->push(
-				new SendCustomLetterJob([
-					'letter_id' => $createLetterModel->letterModel->id,
-					'user_id'   => $dto->user_id,
-					'emails'    => ArrayHelper::map($dto->emails, static fn($email) => ArrayHelper::getValue($email, 'value')),
-					'subject'   => $dto->subject,
-					'body'      => $dto->body,
-					'ways'      => $dto->ways
-				])
-			);
+//			$this->queue->push(
+			(new SendCustomLetterJob([
+				'letter_id' => $createLetterModel->letterModel->id,
+				'user_id'   => $dto->user_id,
+				'emails'    => ArrayHelper::map($dto->emails, static fn($email) => ArrayHelper::getValue($email, 'value')),
+				'subject'   => $dto->subject,
+				'body'      => $dto->body,
+				'ways'      => $dto->ways
+			]))->execute(new Queue());
+//			);
 
 			$tx->commit();
 
