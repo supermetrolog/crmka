73470a62439428e799a5ddcefb5abad7eab198d1
nknwns <andrejshaev@gmail.com>
2025-10-02 05:34:42 +0700

    feat: change query base model

--- models/oldDb/OfferMixMapSearch.php
+++ models/oldDb/OfferMixMapSearch.php
@@ -5,60 +5,63 @@ declare(strict_types=1);
 namespace app\models\oldDb;
 
 use app\exceptions\ValidationErrorHttpException;
-use yii\db\ActiveQuery;
 use yii\data\ActiveDataProvider;
-use yii\db\Expression;
-use yii\helpers\ArrayHelper;
+use yii\db\ActiveQuery;
 
 class OfferMixMapSearch extends OfferMixSearch
 {
-    /**
-     * @return ActiveQuery
-     */
-    private function getQuery(): ActiveQuery
-    {
-        $select = [
-            $this->getField('latitude'),
-            $this->getField('longitude'),
-            $this->getField('address'),
-            $this->getField('complex_id'),
-            $this->getField('object_id'),
-            $this->getField('type_id'),
-            $this->getField('original_id'),
-            $this->getField('status'),
-            $this->getField('id'),
-            $this->getField('photos'),
-        ];
+	/**
+	 * @return ActiveQuery
+	 */
+	private function getQuery(): ActiveQuery
+	{
+		$select = [
+			$this->getField('latitude'),
+			$this->getField('longitude'),
+			$this->getField('address'),
+			$this->getField('complex_id'),
+			$this->getField('object_id'),
+			$this->getField('type_id'),
+			$this->getField('original_id'),
+			$this->getField('status'),
+			$this->getField('id'),
+			$this->getField('photos'),
+			$this->getField('area_floor_full')
+		];
 
-        return OfferMix::find()
-            ->search()
-            ->select($select)
-            ->groupBy($this->getField('object_id'));
-    }
+		return \app\models\OfferMix::find()
+		                           ->search()
+		                           ->select($select)
+		                           ->groupBy($this->getField('object_id'));
+	}
 
-    /**
-     * @param array $params
-     * @return ActiveDataProvider
-     * @throws ValidationErrorHttpException
-     */
-    public function search(array $params): ActiveDataProvider
-    {
-        $query = $this->getQuery();
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-            'pagination' => [
-                'defaultPageSize' => 50,
-                'pageSizeLimit' => [0, 50],
-            ],
-        ]);
+	/**
+	 * @param array $params
+	 *
+	 * @return ActiveDataProvider
+	 * @throws ValidationErrorHttpException
+	 */
+	public function search(array $params): ActiveDataProvider
+	{
+		$query = $this->getQuery();
 
-        $this->load($params, '');
-        if (!$this->validate()) {
-            throw new ValidationErrorHttpException('SSSS');
-        }
-        $this->normalizeProps();
-        $this->setFilters($query);
+		$dataProvider = new ActiveDataProvider([
+			'query'      => $query,
+			'pagination' => [
+				'defaultPageSize' => 50,
+				'pageSizeLimit'   => [0, 50],
+			],
+		]);
 
-        return $dataProvider;
-    }
+		$this->load($params, '');
+
+		if (!$this->validate()) {
+			throw new ValidationErrorHttpException('SSSS');
+		}
+
+		$this->normalizeProps();
+		$this->setFilters($query);
+
+		return $dataProvider;
+	}
 }
