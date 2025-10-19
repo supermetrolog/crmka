48cb4379dafa9f3b2ce4e6788055af84c72ed1fe
nknwns <andrejshaev@gmail.com>
2025-10-20 01:47:27 +0700

    feat: create OfferMixMapSearchView, add `offer_state`

--- models/oldDb/OfferMixMapSearch.php
+++ models/oldDb/OfferMixMapSearch.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace app\models\oldDb;
 
 use app\exceptions\ValidationErrorHttpException;
+use app\models\views\OfferMixMapSearchView;
+use yii\base\ErrorException;
 use yii\data\ActiveDataProvider;
 use yii\db\ActiveQuery;
+use yii\db\Expression;
 
 class OfferMixMapSearch extends OfferMixSearch
 {
 	/**
 	 * @return ActiveQuery
+	 * @throws ErrorException
 	 */
 	private function getQuery(): ActiveQuery
 	{
@@ -30,12 +34,20 @@ class OfferMixMapSearch extends OfferMixSearch
 			$this->getField('visual_id'),
 			$this->getField('test_only'),
 			$this->getField('class'),
+			new Expression(sprintf(
+				"(CASE WHEN SUM(CASE WHEN %s = 2 AND %s = 1 THEN 1 ELSE 0 END) > 0 THEN 2 " .
+				"WHEN SUM(CASE WHEN %s = 2 THEN 1 ELSE 0 END) > 0 THEN 1 " .
+				"ELSE 0 END) as offer_state",
+				$this->getField('type_id'), $this->getField('status'),
+				$this->getField('type_id')
+			)),
+			new Expression(sprintf('CASE WHEN %s = 2 THEN 1 ELSE 0 END as has_ti_2', $this->getField('type_id')))
 		];
 
-		return \app\models\OfferMix::find()
-		                           ->search()
-		                           ->select($select)
-		                           ->groupBy($this->getField('object_id'));
+		return OfferMixMapSearchView::find()
+		                            ->search()
+		                            ->select($select)
+		                            ->groupBy($this->getField('object_id'));
 	}
 
 	/**
